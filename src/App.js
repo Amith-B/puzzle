c34a551835879eb4b4faf062708c4d209cdb605d
Amith-B <amithbr6@gmail.com>
2022-03-28 10:24:36 +0530

    prelaod all images

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import "./App.scss";
 import Puzzle from "./components/puzzle";
 import EditorProvider from "./context/puzzleProvider";
+import { useEffect } from "react";
+import initialData from "./context/initialData";
 
 function App() {
+  useEffect(() => {
+    initialData.images.forEach((url) => {
+      if (url) {
+        const img = new Image();
+        img.src = url;
+      }
+    });
+  }, []);
+
   return (
     <EditorProvider>
       <div className="App">
